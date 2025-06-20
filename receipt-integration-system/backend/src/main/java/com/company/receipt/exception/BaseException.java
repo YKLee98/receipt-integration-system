a29aa012fae64fac0611ea68ee5edf0f190b4aa3
YKLee98 <math.makes.you.free@gmail.com>
2025-06-20 20:12:43 +0900

    Create BaseException.java

---
+++ receipt-integration-system/backend/src/main/java/com/company/receipt/exception/BaseException.java
@@ -0,0 +1,20 @@
+// BaseException.java
+package com.company.receipt.exception;
+
+public abstract class BaseException extends RuntimeException {
+    private final String errorCode;
+    
+    public BaseException(String errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+    
+    public BaseException(String errorCode, String message, Throwable cause) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+    
+    public String getErrorCode() {
+        return errorCode;
+    }
+}
